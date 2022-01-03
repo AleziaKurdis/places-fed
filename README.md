ada8b8ba2ba12737781e893ec4d9e9f2ad834cc4
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-01-03 11:31:21 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # places-fed
-Explore application for a federation
+## Prototype to test new ways for the "Explore" app. in a federation logic.
+With objective to try new approach regarding the design of the « Explore » application.
+I have built this fully functional prototype to see if some new suggested features really worth it.
+The idea here, is to try it before even consider any changes in the current « Explore » application or in the API side.
+For that reason, it’s more efficient and faster to try it apart before.  
+
+So here’s the experimental application (I named it "Places" to avoid any confusion with the built-in « Explore » app.)
+
+Here's the url of the script to run the application : https://aleziakurdis.github.io/places-fed/places.js  
+(To install in ??? interface: Edit > Running Scripts... Load scripts from url)    
+
+
+Next, you will find the list of the different features or changes that have been put in this prototype and the reasons why they have been designed like this:
+
+## What has been changed compare the "Explore" application:
+
+### Retirement of the "beacon" solution... 
+The beacon solution having too much flaws, it has been decommissionned for this re-built.
+It won't be possible to create new beacon using this application.  
+However, it will continue to list the beacon's places 
+_(It has been very useful for in early time. Thank you)_
+
+
+### Place API data extraction...
+The extraction of the Place API data is done from the .js side, rather than the UI layer (.html).
+Doing this, allows to exclude the places that are not compatible with the protocol version use by Vircadia's Interface. 
+(It is not filtered out in the "Explore" app.)
+It also keeps the logic separated from the UI layer, which can be useful to anyone who want to simply redesign the UI.
+
+But that's for this prototype. In the future, the "Explore" app. is appealed to be redesign a couple of time again, 
+when the number of places will become too high and certainaly another time when Vircadia will get decentralized.
+The data processing will need at some point to happen in the API it-self to really scale.
+So the "how to do it" will changed, but the "what it needs to do" might stay valid. _(A good reason to start to define now the "what it needs to do")_
+
+### Removal of the pager solution...
+The pager has been replaced by a scrollable list, with a "Load More..." button.
+A "sidewalk" area next to the scrollbar has been inserted for a comfortable usage in HMD mode.
+
+### Current location address...
+The current location address is displayed at the footer of the "Places" application. 
+This is also for HMD, since we can't see the desktop window header and there was nowhere we could see that info.
+
+### The good things has been kept...
+The navigation buttons, the search/address bar, the place entries with the blurry picture. Those things were good so they have been reused.
+
+### Presence of Life...
+Since it was clearly a problem to get the number of user for a specific place without any potential cheating.
+This concept has simply been evacuated in this design. It has been replaced, instead, by a "presence of life " indicator.
+The domain for it's part has the number of attendance.
+So you know that on a specific place, that you can expect to reach someone, but it could be somewhere else in the domain.
+The number of user or the presence of life is also no more criteria to be in top of the list.
+Some people in a domain is definitely not an indicator that there is a party that you can join.
+
+### There are 2 places list...
+Because there was different needs, two list formats are available.
+
+#### "Explore" (that you get at the opening of the application)
+This list aims to make discover places and figure where the interesting event could be.
+The place with a picture will appears at the first part of the list, follow by those that doesn't have one.
+Setting a picture on a place implicitly means that you do something to make it more attractive. So we can expect that those place might have something to visit.
+At the opposite, places without picture are those that don't expect visitor as in a museum. 
+They are publicly open, but those place are workplace, test environment, in setup or just too private to be advertised.
+Withing these 2 groups, each place get a seeded random ordering. 
+This is to give a chance to ever place to be sometime at the top of the list. (in their respective category)
+The sequence is random, but it get changed only every 5 days. 
+It would be very annoying if the list was completely shuffled each time you open the application, while you get through the list.
+The beauty of the seeded random is that everyone has the same chances, so it gets rid of any idiotic race to be at the top: 
+like artificial count inflation or place name starting with AAA, AAAA or AAAAA to appear at first. (We saw that a lot before. In Second Life specifically.)
+
+#### "By Domains" 
+This list offer a more classic view. 
+It's the list of the domain, ordered by number of attendance, then alphabetically, with their places listed below.
+In that list you can see the count of user at the domain level. (This is reflecting what it is truly, with less concerns that what we have with the "Explore" app.)
+It keeps thing transparent. Now you see the real amount of people.
+
+### Place entries...
+The place entries are now displaying the first line of the description instead of the manager name.
+This information is clearly more useful to figure what the place is, even if we have only the 55 first character, this seems to work fine in most of the case.
+Each place entry has now a link to open a "Place Detail" panel that open full page, with the picture, the description, and all the other important information: Title, owner, maturity level, domain name, count and capacity.
+
+### Filters...
+Both list can be filtered to display only the place that have "presence of life". 
+You can also filter by maturity level.
+
+### Technology...
+This version is 100% js/html5/css.
+There are no dependency on 3rd party libraries. Basically it's 3 files with a bunch of icons/picture.
+
---
+++ places.js
@@ -0,0 +1,466 @@
+"use strict";
+//
+//  places.js
+//
+//  Created by Alezia Kurdis, January 1st, 2022.
+//  Copyright 2022, ??? contributors.
+//
+//  Generate an explore app based on the differents source of placename data.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function() {
+    var jsMainFileName = "places.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
+
+    var CURRENT_METAVERSE_SERVER = AccountServices.metaverseServerURL + "/api/v1/places";
+    
+    var dataSources = [
+            CURRENT_METAVERSE_SERVER
+        ];
+        
+    var metaverseServers = [];    
+        
+    
+    var httpRequest = null;
+    var placesData;
+    var portalList = [];
+
+    var nbrPlacesNoProtocolMatch = 0;
+    var nbrPlaceProtocolKnown = 0;
+    
+    var APP_NAME = "PLACES";
+    var APP_URL = ROOT + "places.html";
+    var APP_ICON_INACTIVE = ROOT + "icons/appicon_i.png";
+    var APP_ICON_ACTIVE = ROOT + "icons/appicon_a.png";
+    var appStatus = false;
+    var channel = "com.vircadia.places"; //ADJUST ###########################################################################    
+
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+    tablet.screenChanged.connect(onScreenChanged);
+
+    var button = tablet.addButton({
+        text: APP_NAME,
+        icon: APP_ICON_INACTIVE,
+        activeIcon: APP_ICON_ACTIVE,
+        sortOrder: 8
+    });
+    
+    var timestamp = 0;
+    var INTERCALL_DELAY = 3000; //3 sec
+    var PERSISTENCE_ORDERING_CYCLE = 5 * 24 * 3600 * 1000; //5 days
+    
+    function clicked(){
+        if (appStatus === true) {
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+            tablet.gotoHomeScreen();
+            appStatus = false;
+        } else {
+            tablet.gotoWebScreen(APP_URL);
+            tablet.webEventReceived.connect(onAppWebEventReceived);
+            appStatus = true;
+        }
+
+        button.editProperties({
+            isActive: appStatus
+        });
+    }
+
+    button.clicked.connect(clicked);
+
+
+    function onAppWebEventReceived(message) {
+        var d = new Date();
+        var n = d.getTime();
+        
+        var messageObj = JSON.parse(message);
+        if (messageObj.channel === channel) {
+            if (messageObj.action === "READY_FOR_CONTENT" && (n - timestamp) > INTERCALL_DELAY) {
+                d = new Date();
+                timestamp = d.getTime();
+                transmitPortalList();
+
+                sendCurrentLocationToUI();
+                
+            } else if (messageObj.action === "TELEPORT" && (n - timestamp) > INTERCALL_DELAY) {
+                d = new Date();
+                timestamp = d.getTime();
+
+                if (messageObj.address.length > 0) {
+                    Window.location = messageObj.address;
+                }                
+                
+            } else if (messageObj.action === "GO_HOME" && (n - timestamp) > INTERCALL_DELAY) {
+                if (LocationBookmarks.getHomeLocationAddress()) {
+                    location.handleLookupString(LocationBookmarks.getHomeLocationAddress());
+                } else {
+                    location.goToLocalSandbox();
+                }                
+            } else if (messageObj.action === "GO_BACK" && (n - timestamp) > INTERCALL_DELAY) {
+                location.goBack();
+            } else if (messageObj.action === "GO_FORWARD" && (n - timestamp) > INTERCALL_DELAY) {
+                location.goForward();
+            }
+        }
+    }
+
+    function onHostChanged(host) {
+        sendCurrentLocationToUI();
+    }
+
+    location.hostChanged.connect(onHostChanged);
+    
+    function sendCurrentLocationToUI() {
+        var currentLocationMessage = {
+            "channel": channel,
+            "action": "CURRENT_LOCATION",
+            "data": location.href
+        };
+
+        tablet.emitScriptEvent(currentLocationMessage);        
+    }
+    
+    function onScreenChanged(type, url) {
+        if (type == "Web" && url.indexOf(APP_URL) != -1) {
+            appStatus = true;
+        } else {
+            appStatus = false;
+        }
+        
+        button.editProperties({
+            isActive: appStatus
+        });
+    }
+
+    function transmitPortalList() {
+        metaverseServers = [];
+        buildMetaverseServerList();
+        
+        portalList = [];
+        nbrPlacesNoProtocolMatch = 0;
+        nbrPlaceProtocolKnown = 0;
+        var extractedData;
+        
+        for (var i = 0; i < dataSources.length; i++ ) {
+            extractedData = getContent(dataSources[i] + "?status=online" + "&acash=" + Math.floor(Math.random() * 999999));
+            try {
+                placesData = JSON.parse(extractedData);
+            } catch(e) {
+                placesData = {};
+            }        
+            httpRequest = null; 
+            processData();
+        }
+
+        //################### TO REMOVED ONCE NO MORE USED #####################
+        getDeprecatedBeaconsData();
+        //################### END: TO REMOVED ONCE NO MORE USED #####################
+        
+        addUtilityPortals();
+        
+        portalList.sort(sortOrder);
+        
+        var percentProtocolRejected = Math.floor((nbrPlacesNoProtocolMatch/nbrPlaceProtocolKnown) * 100);
+        
+        var warning = "";
+        if (percentProtocolRejected > 50) {
+            warning = "WARNING: " + percentProtocolRejected + "% of the places are not listed because they are running under a different protocol. Maybe consider to upgrade.";
+        }
+
+        var message = {
+            "channel": channel,
+            "action": "PLACE_DATA",
+            "data": portalList,
+            "warning": warning
+        };
+
+        tablet.emitScriptEvent(message);
+        
+    };
+
+    function buildMetaverseServerList () {
+
+        //get federation list
+        
+        //var fedDirectoryUrl = AccountServices.metaverseServerURL + "/federation.json"; //adjust if this is developped
+        var fedDirectoryUrl = ROOT + "federation.json";
+        
+        var extractedFedData = getContent(fedDirectoryUrl);
+        print("FEDERATION: " + JSON.stringify(extractedFedData));
+        
+        //mark the connected as local, and as fetching
+        
+        //add the pinned if some to add
+        
+        //marked fetched as fetching
+
+        
+    }
+
+    function getContent(url) {
+        httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", url, false); // false for synchronous request
+        httpRequest.send( null );
+        return httpRequest.responseText;
+    }
+
+    function processData(){
+        var supportedProtocole = Window.protocolSignature();
+   
+        var places = placesData.data.places;
+        for (var i = 0;i < places.length; i++) {
+
+            var category, accessStatus, eventTypeDescription;
+            
+            var description = (places[i].description ? places[i].description : "");
+            var thumbnail = (places[i].thumbnail ? places[i].thumbnail : "");
+
+            if ( places[i].domain.protocol_version === supportedProtocole ) {
+                  
+
+                    if ( thumbnail.substr(0, 4).toLocaleLowerCase() !== "http") {
+                        category = "O"; //Other
+                    } else {
+                        category = "A"; //Attraction                        
+                    }
+                    
+                    if (places[i].domain.num_users > 0) {
+                        if (places[i].domain.num_users >= places[i].domain.capacity && places[i].domain.capacity !== 0) {
+                            accessStatus = "FULL";
+                        } else {
+                            accessStatus = "LIFE";
+                        }
+                    } else {
+                        accessStatus = "NOBODY";
+                    }                 
+
+                    var portal = {
+                        "order": category + "_" + getSeededRandomForString(places[i].id),
+                        "category": category,
+                        "accessStatus": accessStatus,
+                        "name": places[i].name,
+                        "description": description,
+                        "thumbnail": thumbnail,
+                        "maturity": places[i].maturity,
+                        "address": places[i].address,
+                        "current_attendance": places[i].domain.num_users,
+                        "id": places[i].id,
+                        "visibility": places[i].visibility,
+                        "capacity": places[i].domain.capacity,
+                        "tags": getListFromArray(places[i].tags),
+                        "managers": getListFromArray(places[i].managers),
+                        "domain": places[i].domain.name,
+                        "domainOrder": aplphabetize(zeroPad(places[i].domain.num_users, 6)) + "_" + places[i].domain.name + "_" + places[i].name
+                    };
+                    portalList.push(portal);
+
+            } else {
+                nbrPlacesNoProtocolMatch++;
+            }
+        }
+        
+        nbrPlaceProtocolKnown = nbrPlaceProtocolKnown + places.length;
+    
+    }
+
+    //################### CODE TO REMOVED ONCE NO MORE USED #####################
+    function getDeprecatedBeaconsData() {
+        var url = "https://metaverse.vircadia.com/interim/d-goto/app/goto.json";
+        httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", url, false); // false for synchronous request
+        httpRequest.send( null );
+        var extractedData = httpRequest.responseText;
+        
+        httpRequest = null;
+        
+        var places;
+        try {
+            places = JSON.parse(extractedData);
+        } catch(e) {
+            places = {};
+        }
+
+        for (var i = 0;i < places.length; i++) {
+
+            var category, accessStatus, eventTypeDescription;
+            
+            var description = "...";
+            var thumbnail = "";
+            category = "U"; //uncertain
+
+            if (places[i].People > 0) {
+                accessStatus = "LIFE";
+            } else {
+                accessStatus = "NOBODY";
+            }                 
+            
+            var shortenName = places[i]["Domain Name"].substr(0, 24);
+            
+
+            var portal = {
+                "order": category + "_" + getSeededRandomForString(places[i]["Domain Name"]),
+                "category": category,
+                "accessStatus": accessStatus,
+                "name": shortenName,
+                "description": description,
+                "thumbnail": thumbnail,
+                "maturity": "unrated",
+                "address": places[i].Visit,
+                "current_attendance": places[i].People,
+                "id": "BEACON" + i,
+                "visibility": "open",
+                "capacity": 0,
+                "tags": "",
+                "managers": places[i].Owner,
+                "domain": "UNKNOWN (Beacon)",
+                "domainOrder": "ZZZZZZZZZZZZZUA"
+            };
+            portalList.push(portal);
+        }
+    }
+    //################### END::: CODE TO REMOVED ONCE NO MORE USED #####################
+
+    function addUtilityPortals() {
+        var localHostPortal = {
+            "order": "Z_AAAAAA",
+            "category": "Z",
+            "accessStatus": "NOBODY",
+            "name": "localhost",
+            "description": "",
+            "thumbnail": "",
+            "maturity": "unrated",
+            "address": "localhost",
+            "current_attendance": 0,
+            "id": "",
+            "visibility": "open",
+            "capacity": 0,
+            "tags": "",
+            "managers": "",
+            "domain": "",
+            "domainOrder": "ZZZZZZZZZZZZZZA"
+        };
+        portalList.push(localHostPortal);
+
+        var tutorialPortal = {
+            "order": "Z_AAAAAZ",
+            "category": "Z",
+            "accessStatus": "NOBODY",
+            "name": "tutorial",
+            "description": "",
+            "thumbnail": "",
+            "maturity": "unrated",
+            "address": "file:///~/serverless/tutorial.json",
+            "current_attendance": 0,
+            "id": "",
+            "visibility": "open",
+            "capacity": 0,
+            "tags": "",
+            "managers": "",
+            "domain": "",
+            "domainOrder": "ZZZZZZZZZZZZZZZ"
+        };
+        portalList.push(tutorialPortal);
+        
+    }
+
+    function aplphabetize(num) {
+        var numbstring = num.toString();
+        var newChar = "JIHGFEDCBA";
+        var refChar = "0123456789";
+        var processed = "";
+        for (var j=0; j < numbstring.length; j++) {
+            processed = processed + newChar.substr(refChar.indexOf(numbstring.charAt(j)),1);
+        }
+        return processed;
+    }
+
+    function getListFromArray(dataArray) {
+        var dataList = "";
+        if (dataArray !== undefined && dataArray.length > 0) {
+            for (var k = 0; k < dataArray.length; k++) {
+                if (k !== 0) {
+                    dataList += ", "; 
+                }
+                dataList += dataArray[k];
+            }
+            if (dataArray.length > 1){
+                dataList += ".";
+            }
+        }
+        
+        return dataList;
+    }
+
+    function sortOrder(a, b) {
+        var orderA = a.order.toUpperCase();
+        var orderB = b.order.toUpperCase();
+        if (orderA > orderB) {
+            return 1;    
+        } else if (orderA < orderB) {
+            return -1;
+        }
+        if (a.order > b.order) {
+            return 1;    
+        } else if (a.order < b.order) {
+            return -1;
+        }
+        return 0;
+    }
+
+    function zeroPad(num, places) {
+        var zero = places - num.toString().length + 1;
+        return Array(+(zero > 0 && zero)).join("0") + num;
+    }
+
+    function getFrequentPlaces(list) {
+        var count = {};
+        list.forEach(function(list) {
+            count[list] = (count[list] || 0) + 1;
+        });
+        return count;
+    }
+
+    //####### seed random library ################
+    Math.seed = 75;
+
+    Math.seededRandom = function(max, min) {
+        max = max || 1;
+        min = min || 0;
+        Math.seed = (Math.seed * 9301 + 49297) % 233280;
+        var rnd = Math.seed / 233280;
+        return min + rnd * (max - min);
+    }
+
+    function getStringScore(str) {
+        var score = 0;
+        for (var j = 0; j < str.length; j++){
+            score += str.charAt(j).charCodeAt(0) + 1;
+        }
+        return score;
+    }
+
+    function getSeededRandomForString(str) {
+        var score = getStringScore(str);
+        var d = new Date();
+        var n = d.getTime();
+        var currentSeed = Math.floor(n / PERSISTENCE_ORDERING_CYCLE);
+        Math.seed = score * currentSeed;
+        return zeroPad(Math.floor(Math.seededRandom() * 100000),5);
+    }
+    //####### END of seed random library ################
+
+    function cleanup() {
+
+        if (appStatus) {
+            tablet.gotoHomeScreen();
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+        }
+
+        tablet.screenChanged.disconnect(onScreenChanged);
+        tablet.removeButton(button);
+    }
+
+    Script.scriptEnding.connect(cleanup);
+}());
